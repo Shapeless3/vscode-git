4e24c532fb47ee2af08dbd91ab5267d804a1cfaa
Shapeless <stopher.osborne.@gmail.com>
2018-05-31 07:48:24 -0700

    Close to final on remove-oldfiles

--- Remove-OldFiles.ps1
+++ Remove-OldFiles.ps1
@@ -1,8 +1,25 @@
-$limit = (Get-Date).AddDays(-15)
-$path = "C:\Some\Path"
+#Script has a default limit and path.  Change for deployment in automation.
 
-# Delete files older than the $limit.
-Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+[cmdletbinding()]
+param(
+    [Parameter()]
+    [int]$daysold = 45,
+    [Parameter()]
+    $path = 'C:\Temp'
+)
 
-# Delete any empty directories left behind after deleting the old files.
-Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
+begin {
+    $limit = (Get-Date).AddDays( - $daysold)
+}
+
+process {
+    # Delete files older than the $limit.  First line uses creation date, second uses last modified date.
+    #Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force -WhatIf
+    Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force -WhatIf
+
+    # Delete any empty directories left behind after deleting the old files.
+    Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse -WhatIf
+}
+
+end
+{}
